569be32f180243434eb8b6f2468f9cef8caaac8d
Евгений <142923731+HelloEveryNyan@users.noreply.github.com>
2023-09-07 10:43:51 +0200

    Add files via upload
    Задание 4

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.Write("Четные числа от 1 до N: ");
+        
+        for (int i = 2; i <= N; i += 2)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
