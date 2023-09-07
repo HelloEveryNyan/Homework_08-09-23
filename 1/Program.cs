0ce806bfe533cdf50257ce77083a26506eb96203
Евгений <142923731+HelloEveryNyan@users.noreply.github.com>
2023-09-07 10:25:05 +0200

    Add files via upload
    Задание 1

---
+++ 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число (A): ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число (B): ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a > b)
+        {
+            Console.WriteLine($"{a} больше, чем {b}");
+            Console.WriteLine($"{b} меньше, чем {a}");
+        }
+        else if (a < b)
+        {
+            Console.WriteLine($"{b} больше, чем {a}");
+            Console.WriteLine($"{a} меньше, чем {b}");
+        }
+        else
+        {
+            Console.WriteLine($"{a} и {b} равны");
+        }
+    }
+}
