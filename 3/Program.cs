cda7ee469d7fe6a45f091f007341810b22a60659
Евгений <142923731+HelloEveryNyan@users.noreply.github.com>
2023-09-07 10:41:05 +0200

    Add files via upload
    Задание 3

---
+++ 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Число является четным.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является четным.");
+        }
+    }
+}
